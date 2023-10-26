c7c7fefd3bb70fa05ec9a6e5ded509c30d7325ab
PanariRasul <panarirasul@gmail.com>
2023-10-27 00:49:05 +0530

    update API for Specific book category language in index.js

--- index.js
+++ index.js
@@ -18,6 +18,68 @@ booky.get("/", (req, res) => {
 })
 
 
+/*
+Route           /is
+Description     Get specific book on ISBN
+Access          PUBLIC
+Parameter       ISBN
+Methods         GET
+*/
+
+booky.get("/is/:isbn", (req, res) => {
+    const getSpecificBook  = database.books.filter (
+        (book) => book.ISBN === req.params.isbn
+    );
+
+    if(getSpecificBook.length === 0) {
+        return res.json({error:`No Book Found for the ISBN of ${req.params.isbn}`})
+    }
+
+    return res.json({book: getSpecificBook});
+})
+
+/*
+Route           /is
+Description     Get specific book on category
+Access          PUBLIC
+Parameter       category
+Methods         GET
+*/
+
+booky.get("/c/:category",(req,res) => {
+    const getSpecificBook = database.books.filter (
+        (book) => book.category.includes(req.params.category)
+    );
+
+    if(getSpecificBook.length === 0) {
+        return res.json({error:`No Book found for the category of ${req.params.category}`})
+    }
+
+    return res.json({book: getSpecificBook});
+});
+
+/*
+Route           /is
+Description     Get specific book on language
+Access          PUBLIC
+Parameter       language
+Methods         GET
+*/
+
+booky.get ("/lan/:language",(req,res) => {
+    const getSpecificBook = database.books.filter (
+        (book) => book.language.includes(req.params.language)
+    );
+
+    if(getSpecificBook.length === 0) {
+        return res.json({error:`No Book found for the language of ${req.params.language}`})
+    }
+
+    return res.json({book: getSpecificBook})
+})
+
+
+
 booky.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
